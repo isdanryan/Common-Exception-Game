4fcbef31584749d5e929cfb8d0fda71c69a7a59c
Dan Ryan <isdanryan@gmail.com>
2024-11-09 19:03:05 +0000

    Separate function
    Separate getRandomWord function into it's own function.
    Update replaceChar function so it also returns the missing letter

--- src/App.js
+++ src/App.js
@@ -2,36 +2,42 @@ import { useState, useEffect } from "react";
 import word_lists from './words.json';
 
 function replaceChar(str, index) {
-  return str.substring(0, index) + "_" + str.substring(index + 1);
+  const blankedWord =  str.substring(0, index) + "_" + str.substring(index + 1);
+  const missingLetter = str.charAt(index);
+  console.log("Letter missing is:", missingLetter);
+  const generatedWord = [blankedWord, missingLetter];
+  return generatedWord;
+}
+
+function getRandomWord(word_lists) {
+  const words = word_lists.year_2;
+  const randomIndex = Math.floor(Math.random() * words.length); // Get a random number from within the words list
+  const selectedWord = words[randomIndex];
+  console.log("Word is:", selectedWord);
+  const randomPlacement = Math.floor(Math.random() * selectedWord.length); // Get a random number within the word length
+  console.log("Position to blank is:", randomPlacement);
+  const wordArray = replaceChar(selectedWord, randomPlacement);
+  const word = wordArray[0];
+  const letter = wordArray[1];
+  console.log("Finale word is:", word);
+  console.log("Missing letter is:", letter);
+  return word; // Return the word at the position of the random number
 }
 
 
 function App() {
   const [word, setWord] = useState("");
 
-  // Function to get a random word
-  const getRandomWord = () => {
-    const words = word_lists.year_2;
-    const randomIndex = Math.floor(Math.random() * words.length); // Get a random number from within the words list
-    const selectedWord = words[randomIndex];
-    console.log("Word is:", selectedWord);
-    const randomPlacement = Math.floor(Math.random() * selectedWord.length); // Get a random number within the word length
-    console.log("Position to blank is:", randomPlacement);
-    const generatedWord = replaceChar(selectedWord, randomPlacement)
-    console.log("FInale word is:", generatedWord);
-    return generatedWord; // Return the word at the position of the random number
-  };
-
   // Get the random word when the component loads
   useEffect(() => {
-    setWord(getRandomWord());
+    setWord(getRandomWord(word_lists));
   }, []);
 
   return (  
     <div className='game-area max-w-xl'>
       <header>
         <h1 className='title'>Common Exception Word Game</h1>
-        <p>Can you guess the word?</p>
+        <p>Can you guess the missing letter?</p>
       </header>
 
       <div className='display-area'>
