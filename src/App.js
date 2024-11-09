1baceee3177f9cc6e8e345494d82e5b761742855
Dan Ryan <isdanryan@gmail.com>
2024-11-09 18:28:42 +0000

    Drop leter from word
    Create function to randomly drop a letter from the selected word

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import word_lists from './words.json';
 
+function replaceChar(str, index) {
+  return str.substring(0, index) + "_" + str.substring(index + 1);
+}
+
 
 function App() {
   const [word, setWord] = useState("");
@@ -8,8 +12,14 @@ function App() {
   // Function to get a random word
   const getRandomWord = () => {
     const words = word_lists.year_2;
-    const randomIndex = Math.floor(Math.random() * words.length) // Get a random number from within the words list
-    return words[randomIndex]; // Return the word at the position of the random number
+    const randomIndex = Math.floor(Math.random() * words.length); // Get a random number from within the words list
+    const selectedWord = words[randomIndex];
+    console.log("Word is:", selectedWord);
+    const randomPlacement = Math.floor(Math.random() * selectedWord.length); // Get a random number within the word length
+    console.log("Position to blank is:", randomPlacement);
+    const generatedWord = replaceChar(selectedWord, randomPlacement)
+    console.log("FInale word is:", generatedWord);
+    return generatedWord; // Return the word at the position of the random number
   };
 
   // Get the random word when the component loads
