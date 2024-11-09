c5b83eb2a080450ea47a7821ff2e9dca3c2e5113
Dan Ryan <isdanryan@gmail.com>
2024-11-09 18:09:28 +0000

    get random word
    Add json file containing lists of common exception words
    Add function to get a random word form the json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react";
+import word_lists from './words.json';
+
 
 function App() {
-  return (
+  const [word, setWord] = useState("");
+
+  // Function to get a random word
+  const getRandomWord = () => {
+    const words = word_lists.year_2;
+    const randomIndex = Math.floor(Math.random() * words.length) // Get a random number from within the words list
+    return words[randomIndex]; // Return the word at the position of the random number
+  };
+
+  // Get the random word when the component loads
+  useEffect(() => {
+    setWord(getRandomWord());
+  }, []);
+
+  return (  
     <div className='game-area max-w-xl'>
       <header>
         <h1 className='title'>Common Exception Word Game</h1>
@@ -8,7 +25,7 @@ function App() {
       </header>
 
       <div className='display-area'>
-        <p>WORD GOES HERE</p>
+        {word}
       </div>
 
       <div className='letter-area'>
